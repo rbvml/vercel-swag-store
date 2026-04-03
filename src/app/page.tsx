5bf39a51d5d633b4e578316a25a95a8a7283e440
Riccardo Bartoli <riccardo.bartoli@vml.com>
2026-04-03 15:34:32 +0200

    feat: add promo banner to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import Hero from "@/components/hero";
+import PromoBanner, { PromoBannerSkeleton } from "@/components/promo-banner";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <>
+      <Suspense fallback={<PromoBannerSkeleton />}>
+        <PromoBanner />
+      </Suspense>
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <Hero />
       </div>
