ceaceb7d2f1ac3116bd1c8c2e5d7b2528762e971
Riccardo Bartoli <riccardo.bartoli@vml.com>
2026-04-06 11:11:13 +0200

    feat: add metadata to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,15 @@ import Hero from "@/components/hero";
 import PromoBanner, { PromoBannerSkeleton } from "@/components/promo-banner";
 import FeaturedProducts from "@/components/featured-products";
 import { Suspense } from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "Vercel Swag Store",
+  },
+  description:
+    "Official Vercel merchandise. Premium developer apparel, accessories, and gear.",
+};
 
 export default function Home() {
   return (
